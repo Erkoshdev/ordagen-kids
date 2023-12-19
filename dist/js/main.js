2808bd574aecccbcdf158c129a8f4e1f628c864a
Safiolla <yerkebulan.safiolla@gmail.com>
2023-12-19 06:07:40 +0600

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,76 @@
+//promo slider
+const promoSlider = new Swiper(".promo-slider", {
+  slidesPerView: 1,
+  loop: true,
+  navigation: {
+    nextEl: ".promo-slider-next",
+    prevEl: ".promo-slider-prev",
+  },
+});
+
+
+//review slider
+const reviewSlider = new Swiper('.review-slider', {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 30,
+  pagination: {
+    el: '.review-pagination',
+    type: 'bullets',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.review-slider-next',
+    prevEl: '.review-slider-prev',
+  },
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+  }
+});
+
+
+//mask
+$(document).ready(function() {
+  $('.phone-mask').mask('+7(000)000-00-00');
+})
+
+
+//toggle mobile menu
+$('.toggle-mobile-menu').click(function () {
+  $(this).toggleClass('active');
+  $('.mobile-menu').toggleClass('show');
+  $('body').toggleClass('scroll-locked')
+})
+
+
+//monitoring slider
+$(document).ready(function () {
+  if($(window).width() < 768) {
+    const monitoringSlider = new Swiper('.monitoring-slider', {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      navigation: {
+        nextEl: '.monitoring-slider-next',
+        prevEl: '.monitoring-slider-prev',
+      },
+    });
+  }
+})
+
+
+//toggle header nav on tablet
+$('.toggle-header-nav').click(function () {
+  $(this).toggleClass('active');
+  $('.header-nav').toggleClass('show')
+})
+
+$(document).mouseup(function(e) {
+  var container = $(".header-nav, .toggle-header-nav");
+  if (!container.is(e.target) && container.has(e.target).length === 0) {
+    $('.header-nav').removeClass('show');
+    $('.toggle-header-nav').removeClass('active')
+  }
+});
